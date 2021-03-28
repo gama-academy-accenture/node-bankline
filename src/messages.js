160ee456c72c12c01bbd368c502f6686a926b1ac
j-bittenbinder <juliocbittenbinder@gmail.com>
2021-03-28 18:22:32 -0300

    pushing class project and adding lib cpf-validator

---
+++ src/messages.js
@@ -0,0 +1,28 @@
+//export default {
+module.exports = {
+    Pessoa:{
+        NomeNulo: "Nome não informado",
+        CPFNulo: "CPF não informado",
+        CPFInvalido: "CPF inválido",
+        UsuarioTipoErrado: "O tipo do parametro 'usuario' está incorreto",
+        ContaCorrenteTipoErrado: "O tipo do parametro 'conta-corrente' está incorreto",
+        JaExiste: "Pessoa já existe",
+        NaoEncotrada: "Pessoa não encontrada"
+    },
+    Operacao:{
+        TipoInvalido: "Tipo de operação inválida",
+        ContaCorrenteOrigemNulo: "Conta corrente origem não informada",  
+        ValorInvalido: "Valor de operação é inválido",
+        ContaCorrenteDestinoNula: "Conta corrente destino não informada",
+        ContaOrigemIgualDestino: "Conta corrente origem é igual a conta destino",
+        SaldoInsuficiente: "Saldo Insuficiente"
+    },
+    Usuario:{
+        LoginNulo: "Login não informado",
+        SenhaNula: "Senha não informada",
+        SenhaInvalida: "Digite uma senha válida"
+    },
+    ContaCorrente:{
+        NaoEncontrada: "Conta-corrente não encontrada",
+    }
+}
